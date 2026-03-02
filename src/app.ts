bb2521431c0965af360a2b26799bcfb166e1d71c
Utsho MH <utshomh.dev@gmail.com>
2026-03-02 21:29:44 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 
 import authRouter from "./modules/auth/auth.router";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
